b52bebfcdb5df97bf7b54770d96f3b4c1a334b8a
xujing <xujing-sj@bestpay.com.cn>
2020-05-15 10:42:49 +0800

    'update'

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)).split('write_yaml')[0])
 
 
-write_yaml('4554665')
-write1_yaml('56rrggrg')
+write_yaml('iefhjdff')
+write1_yaml('fhruh')
